adda981b35c6fdba242a96700a1899b1eeb41f3b
arfes0e2b3c <webyata@gmail.com>
2022-04-03 18:27:00 +0900

    proxy true

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: "/",
-    // proxy: true,
+    proxy: true,
     prefix: "/",
   },
 
