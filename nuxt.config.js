4390d6d9f11f833552aba947c1b541601a44c0ba
arfes0e2b3c <webyata@gmail.com>
2022-04-05 15:58:46 +0900

    reset to yesterday

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   loading: true,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "q-box",
+    title: "Q-BOX",
     htmlAttrs: {
       lang: "ja",
     },
