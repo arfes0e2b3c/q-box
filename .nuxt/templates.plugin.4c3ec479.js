c6bc40ead69d8a8ae57e845ebd880a064a7eb087
arfes0e2b3c <webyata@gmail.com>
2023-01-31 22:01:37 +0900

    キャッシュの削除

--- .nuxt/templates.plugin.4c3ec479.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import { library, config } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
-
-config.autoAddCss = false
-
-Vue.component('fa', FontAwesomeIcon)
-Vue.component('fa-layers', FontAwesomeLayers)
-Vue.component('fa-layers-text', FontAwesomeLayersText)
