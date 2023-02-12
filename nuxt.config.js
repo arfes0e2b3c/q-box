790e2e4f7a736150850286c896c5092713295280
arfes0e2b3c <webyata@gmail.com>
2023-02-12 11:37:23 +0900

    Revert "firebaseの設定ファイルを追加"
    This reverts commit 15bbda5be58fc98cb9c7d92f8eeb8d5384cd3628.

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,26 +64,8 @@ export default {
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
     "@nuxtjs/proxy",
-    "@nuxtjs/firebase",
     ["vue-scrollto/nuxt", { duration: 300 }],
     "nuxt-fontawesome",
-    [
-      "@nuxtjs/firebase",
-      {
-        config: {
-          apiKey: "AIzaSyAMy3NDOwW-Q10n1NnDvbyHlOnk40AkcmQ",
-          authDomain: "q-box-ynu.firebaseapp.com",
-          projectId: "q-box-ynu",
-          storageBucket: "q-box-ynu.appspot.com",
-          messagingSenderId: "204406918432",
-          appId: "1:204406918432:web:0e42048dd2405410729b45",
-          measurementId: "G-W0BDPNL92B",
-        },
-        services: {
-          auth: true, // Just as example. Can be any other service.
-        },
-      },
-    ],
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -96,10 +78,6 @@ export default {
 
   fontawesome: {
     component: "fa",
-  },
-
-  router: {
-    mode: "hash",
   },
 
   proxy: {
