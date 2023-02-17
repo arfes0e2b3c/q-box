b29283a8b546d424e14e8e3041ca84258bcc2a2f
arfes0e2b3c <webyata@gmail.com>
2023-02-17 14:16:15 +0900

    管理者側ヘッダーをコンポーネント化

---
+++ components/shared/AnswerHeader.vue
@@ -0,0 +1,90 @@
+<template>
+  <header>
+    <nuxt-link to="/" class="nuxt-link">質問箱を見に行く</nuxt-link>
+    <h1
+      v-scroll-to="{
+        element: '#app',
+        offset: -200,
+        duration: 500,
+      }"
+    >
+      質問箱(管理者版)
+    </h1>
+    <button @click="changeShowMode">画面切り替え</button>
+  </header>
+</template>
+<script>
+export default {
+  methods: {
+    changeShowMode() {
+      this.$emit("chageShowMode");
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+header {
+  width: 100%;
+  height: 70px;
+  background-color: white;
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 0 10px 5px rgba(0, 0, 0, 0.2);
+  z-index: 1000;
+  h1 {
+    cursor: pointer;
+  }
+  .nuxt-link {
+    width: 20%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.1);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #333;
+    text-decoration: none;
+    transition: 0.5s;
+    &:hover {
+      background-color: rgba(48, 48, 48, 1);
+      color: white;
+    }
+  }
+
+  button {
+    width: 20%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.1);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #333;
+    text-decoration: none;
+    transition: 0.5s;
+    border: none;
+    cursor: pointer;
+
+    &:hover {
+      background-color: rgba(48, 48, 48, 1);
+      color: white;
+    }
+  }
+}
+@media (max-width: 520px) {
+  header {
+    height: 60px;
+    h1 {
+      display: none;
+    }
+    .nuxt-link {
+      width: 50%;
+    }
+    button {
+      border-left: 1px solid rgb(200, 200, 200);
+      width: 50%;
+    }
+  }
+}
+</style>
--- pages/answer.vue
+++ pages/answer.vue
@@ -1,18 +1,6 @@
 <template>
   <div id="app">
-    <header>
-      <nuxt-link to="/" class="nuxt-link">質問箱を見に行く</nuxt-link>
-      <h1
-        v-scroll-to="{
-          element: '#app',
-          offset: -200,
-          duration: 500,
-        }"
-      >
-        質問箱(管理者版)
-      </h1>
-      <button @click="changeShowMode">画面切り替え</button>
-    </header>
+    <SharedAnswerHeader @chageShowMode="changeShowMode" />
     <AnswerWaitPost v-show="showPost" class="answer-wait-post" />
     <AnswerWaitReply v-show="showReply" class="answer-wait-reply" />
     <AnswerKeepPost v-show="showKeep" class="answer-keep-post" />
@@ -59,76 +47,10 @@ export default {
 </style>
 <style lang="scss" scoped>
 #app {
-  header {
-    width: 100%;
-    height: 70px;
-    background-color: white;
-    position: fixed;
-    top: 0;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    box-shadow: 0 0 10px 5px rgba(0, 0, 0, 0.2);
-    z-index: 1000;
-    h1 {
-      cursor: pointer;
-    }
-    .nuxt-link {
-      width: 20%;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.1);
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      color: #333;
-      text-decoration: none;
-      transition: 0.5s;
-      &:hover {
-        background-color: rgba(48, 48, 48, 1);
-        color: white;
-      }
-    }
-
-    button {
-      width: 20%;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.1);
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      color: #333;
-      text-decoration: none;
-      transition: 0.5s;
-      border: none;
-      cursor: pointer;
-
-      &:hover {
-        background-color: rgba(48, 48, 48, 1);
-        color: white;
-      }
-    }
-  }
   .answer-wait-reply,
   .answer-wait-post,
   .answer-keep-post {
     margin-top: 90px;
-  }
-}
-@media (max-width: 520px) {
-  #app {
-    header {
-      height: 60px;
-      h1 {
-        display: none;
-      }
-      .nuxt-link {
-        width: 50%;
-      }
-      button {
-        border-left: 1px solid rgb(200, 200, 200);
-        width: 50%;
-      }
-    }
   }
 }
 </style>
