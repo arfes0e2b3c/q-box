7d039e2c0c31fcb9af3f98ff6dad04df233ef60e
arfes0e2b3c <webyata@gmail.com>
2023-02-27 13:31:31 +0900

    nuxt.config.jsの不必要なコメントを削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,9 @@
-require("dotenv").config();
+// require("dotenv").config();
 import axios from "axios";
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: true,
   target: "server",
   loading: false,
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "お手伝いサークル",
     htmlAttrs: {
