76d942be0b6d0d4843c15818876268b5fbbf2293
arfes0e2b3c <webyata@gmail.com>
2023-03-01 03:10:41 +0900

    authenticated.jsからalertを削除

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,7 +1,5 @@
 export default function ({ store, redirect }) {
-  console.log("authenticating");
   if (!store.getters["modules/user/isAuthenticated"]) {
-    console.log("authenticated");
     return redirect("/auth/login");
   }
 }
